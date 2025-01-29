6bf80ad90d9303b83802b45f1dc43d63df59898c
Nusky7 <135059268+Nusky7@users.noreply.github.com>
2025-01-29 17:48:17 +0100

    Improved Gallery

--- script.js
+++ script.js
@@ -18,14 +18,30 @@ document.getElementById('whatsappForm').addEventListener('submit', function(even
     let whatsappURL = `https://wa.me/${phoneNumber}?text=${encodedMessage}`;
     window.open(whatsappURL, '_blank');
 });
-
+    
+    // Swiper de los thumbs
+    const galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 1, 
+        slidesPerView: "auto", 
+        grid: {
+            rows: 2, 
+            fill: 'row', 
+        },
+        freeMode: true, 
+        watchSlidesProgress: false, 
+          breakpoints: {
+        200: { slidesPerView: 2 }, // 5 columnas en pantallas pequeñas
+        640: { slidesPerView: 5 }, // 5 columnas en pantallas pequeñas
+        768: { slidesPerView: 6 }, // 6 columnas en tablets
+        1024: { slidesPerView: 9 }, // 9 columnas en pantallas grandes
+    },
+    });
 
     new Swiper('.swiper-container', {
+        thumbs: {
+        swiper: galleryThumbs, 
+        },
         effect: 'coverflow',
-        grabCursor: true,   
-        spaceBetween: 3,
-        centeredSlides: true, 
-        slidesPerView: "auto",
         coverflowEffect: {
             rotate: 50,   
             stretch: 0,   
@@ -33,31 +49,33 @@ document.getElementById('whatsappForm').addEventListener('submit', function(even
             modifier: 1,
             slideShadows: true, 
         },
+        // grabCursor: true,   
+        spaceBetween: 6,
+        centeredSlides: true, 
+        slidesPerView: "auto",
         autoHeight: true,
-        // loop: true,
+        loop: true,
         slidesPerView: 1,
-        // zoom: {
-        //     limitToOriginalSize: false,
-        //     minRatio: 0.3,
-        //     maxRatio: 2,
-        //     panOnMouseMove: true,
-        //     toggle: true,
-        // },
-        autoplay: {
-            delay: 7000,
-            pauseOnMouseEnter: true
+        zoom: {
+            limitToOriginalSize: false,
+            minRatio: 1,
+            maxRatio: 1.8,
+            panOnMouseMove: true,
+            toggle: true,
         },
-        // thumbs: {
-        //     swiper: galleryThumbs,
-        // },
+        autoplay: {
+            delay: 6000,
+            pauseOnMouseEnter: true,
+        },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
         },
-        // pagination: {
-        //     el: '.swiper-pagination',
-        //     clickable: true,
-        // },
+        pagination: {
+        el: '.swiper-progressbar',
+            type: 'progressbar',
+        
+        },
         breakpoints: {
             640: {
                 slidesPerView: 1,
@@ -66,9 +84,24 @@ document.getElementById('whatsappForm').addEventListener('submit', function(even
                 slidesPerView: 2,
             },
             1500: {
-                slidesPerView: 3,
+                slidesPerView: 4,
             },
         },
     });
-   
+
+      // Capturar el click en cada thumbnail
+    document.querySelectorAll('.gallery-thumbs .swiper-slide').forEach((thumb, index) => {
+        thumb.addEventListener('click', (e) => {
+            e.preventDefault();
+
+            // Desplazarse al contenedor principal
+            const swiperContainer = document.querySelector('.swiper-container');
+            swiperContainer.scrollIntoView({ behavior: 'smooth', block: 'center' });
+
+            // Cambiar a la diapositiva correspondiente después de un pequeño retraso
+            setTimeout(() => {
+                galleryThumbs.slideTo(index);
+            }, 300);
+        });
+    });
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,9 @@ module.exports = {
         },
       keyframes: {
         glowPurple: {
-          '0%': { boxShadow: '0 0 3px #fff' },
-          '50%': { boxShadow: '0 0 5px #fff, 0 0 50px rgb(147 51 234)' },
-          '100%': { boxShadow: '0 0 3px #fff' },
+          '0%': { boxShadow: '0 0 1px #111' },
+          '50%': { boxShadow: '0 0 5px #111, 0 0 50px rgb(147 51 234)' },
+          '100%': { boxShadow: '0 0 1px #111' },
         },
         
       }
